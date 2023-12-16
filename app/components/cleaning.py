5dfc8b58dc63b497ddc5f203410cd6c8ce3ec756
Henry-Tourraine <henry.tourraine@outlook.fr>
2023-12-16 14:25:51 +0100

    ...

--- app/components/cleaning.py
+++ app/components/cleaning.py
@@ -137,6 +137,8 @@ class Cleaning:
         df[colonne].apply(lambda chaine: '/'.join(str(int(x)) for x in chaine.split('/')) if '/' in chaine else chaine)
         #df[colonne].str.replace("\d{2}\/d{2}\/d{4}", r'\1', regex=True)
         def remove_n(df):
+          if len(df[colonne]) < 3:
+              return "31/12/9999"
           temp = df[colonne].split("/")
           print(df)
           df[colonne] = f"{temp[0][-2:]}/{temp[1][-2:]}/{temp[2][-4:]}"
