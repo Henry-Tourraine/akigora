863ab8aef7ff1464ca467e6ebd99577a98358486
Henry-Tourraine <henry.tourraine@outlook.fr>
2023-12-19 17:35:26 +0100

    ...

--- app/components/cleaning.py
+++ app/components/cleaning.py
@@ -145,7 +145,7 @@ class Cleaning:
         ex: 01/010/2260 -> 01/10/2260
         '''
         if int(df.loc[~df[colonne].isna()][colonne][0]) > 1000000: #is timestamp
-                df[colonne] = pd.to_datetime(df[colonne], format="%d/%m/%Y", unit="ms")
+                df[colonne] = pd.to_datetime(df[colonne], format="%d/%m/%Y")
                 return df  
         
         df[colonne].apply(lambda chaine: '/'.join(str(int(x)) for x in chaine.split('/')) if '/' in chaine else chaine)
