5a17533e1af95155f0d8b15860a62217a23278bc
Henry-Tourraine <henry.tourraine@outlook.fr>
2023-12-20 15:28:39 +0100

    ...

--- app/components/cleaning.py
+++ app/components/cleaning.py
@@ -95,7 +95,7 @@ class Cleaning:
         '''
         df[colonne] = df[colonne].astype(str).str.replace("\s+", "")
         df[colonne].replace(to_replace=",", value=".", inplace=True, regex=True)
-        df[df[colonne] == "", colonne] = "NaN"
+        df[df[colonne] == ""] = "NaN"
         df[colonne] = df[colonne].astype("float64")
         return df
 
