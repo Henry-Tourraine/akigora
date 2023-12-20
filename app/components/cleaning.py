682667ab2fc15087c87e3d32828b361a55cf0b71
Henry-Tourraine <henry.tourraine@outlook.fr>
2023-12-20 15:18:31 +0100

    ...

--- app/components/cleaning.py
+++ app/components/cleaning.py
@@ -93,7 +93,7 @@ class Cleaning:
         INPUT: dataframe with column containing numerics in various formats (object...)
         OUTPUT: dataframe with column type as float64
         '''
-        df[colonne] = df[colonne].str.strip()
+        df[colonne] = df[colonne].astype(str).str.strip()
         df[colonne].replace(to_replace=",", value=".", inplace=True, regex=True)
         df[colonne][df[colonne] == ""] = "NaN"
         df[colonne] = df[colonne].astype("float64")
